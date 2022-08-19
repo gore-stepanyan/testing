e8a5e05c7f155503487df11c6eb45737ca57a57e
gore-stepanyan <89501431007and@gmail.com>
2022-08-19 18:00:18 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Your branch is up to date with 'origin/master'.
    
    Changes to be committed:
    	modified:   dev.py

--- dev.py
+++ dev.py
@@ -4,11 +4,17 @@ from packet_handler import PacketHandler
 
 capture = pyshark.FileCapture('session_from_ecss_linphone.pcap', display_filter='sdp')
 
-def get_rtcp(packet):
+def unpack(packet):
     field_names = packet.sip._all_fields
     field_values = packet.sip._all_fields.values()
     return dict(zip(field_names, field_values))
 
+count = 0
+
 for packet in capture:
-    #time.sleep(1)
-    print('\n', get_rtcp(packet), '\n')
+    print(count)
+    sip = unpack(packet)
+    if "sip.Method" in sip:
+        if sip["sip.Method"] == "INVITE":
+            print('method ', sip["sip.Method"], ' catched')
+    count += 1
